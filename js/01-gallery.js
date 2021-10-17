0c06fcfeaed44e5b9f468921d5cb134dc1d6b26b
Viktoriia Huk <relax16011991@gmail.com>
2021-10-17 13:13:58 +0300

    + первая част задачи 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryImages = document.querySelector('.gallery');
+const cardsImage = createGalleryItemCardsImage(galleryItems);
+let originalImage;
+
+galleryImages.insertAdjacentHTML('beforeend', cardsImage);
+
+galleryImages.addEventListener('keydown', onClickEscCloseImage);
+
+function createGalleryItemCardsImage(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+                 />
+             </a>
+        </div>
+        `;
+        })
+        .join('');
+}
+
+function onClickEscCloseImage(evt) {
+if (evt.code !== 'Escape') {
+      return;
+    }
+    originalImage.close();
+}
+
+// galleryImages.addEventListener('click', evt => {
+//     evt.preventDefault();
+//     if (evt.target.nodeName !== "IMG") {
+//         return;
+//     }
+//     originalImage = basicLightbox.create(`
+// 		<img src="${evt.target.dataset.source}" width="1280" height="900">
+// 	`)
+//     originalImage.show()
+// });
+
 console.log(galleryItems);
