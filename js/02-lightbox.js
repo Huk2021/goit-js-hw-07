4a026cd08526b29d4cd99201a728bd2294cd0159
Viktoriia Huk <relax16011991@gmail.com>
2021-10-19 11:33:48 +0300

    добавляет разметку по массиву данных galleryItems , подключает скрипты CDN в задание 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryImages = document.querySelector('.gallery');
+const cardsImage = createGalleryItemCardsImage(galleryItems);
+
+galleryImages.insertAdjacentHTML('beforeend', cardsImage);
+
+function createGalleryItemCardsImage(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+            <a class="gallery__item" href="${original}">
+             <img class="gallery__image"
+             src="${preview}"
+             alt="${description}" />
+            </a>
+        `;
+        })
+        .join('');
+}
+
+
 
 console.log(galleryItems);
